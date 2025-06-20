1ef7aa9a9166c068eb4e5cb2d76c6cf97c60f5b5
Zoe Clark <zoeclark@Zoes-MacBook-Air.local>
2025-06-20 13:46:14 -0400

    Final pre-publish local version

---
+++ rooms/room3/room3_imageList.js
@@ -0,0 +1,13 @@
+const room3images = [
+  "images/room3_1.png",
+  "images/room3_2.png",
+  "images/room3_3.png",
+  "images/room3_4.png",
+  "images/room3_5.png",
+  "images/room3_6.png",
+  "images/room3_7.png",
+  "images/room3_8.png",
+  "images/room3_9.png",
+  "images/room3_10.png",
+  "images/room3_11.png"
+];
